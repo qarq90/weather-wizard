6253345aebc8dd885c759c39819e4ec700f5dd6c
Abdurrahman Qureshi <scythethroughtheheart05@gmail.com>
2023-07-19 23:50:30 +0530

    main app completed

--- src/App.js
+++ src/App.js
@@ -1,29 +1,54 @@
 import React from "react";
 import styled from "styled-components";
+import "./styles/styles.css";
 import Wizard from "./components/Wizard";
-import { FaHatWizard } from "react-icons/fa";
+import wizHat from "../src/img/wizhat.png";
+//import {} from "react-icons/fa6";
 
 function App() {
   return (
-    <div>
+    <StyledApp>
       <StyledTitle>
-        <FaHatWizard className="title-icon" />
+        <StyledImg className="hatA" src={wizHat} alt="wiz-hat" />
         <h1>Weather Wizard</h1>
-        <FaHatWizard className="title-icon" />
+        <StyledImg className="hatB" src={wizHat} alt="wiz-hat" />
       </StyledTitle>
       <Wizard />
-    </div>
+    </StyledApp>
   );
 }
 
+const StyledApp = styled.div`
+  background-color: #32174d;
+  width: 100vw;
+  height: 100vh;
+`;
+
 const StyledTitle = styled.div`
-  padding: 1rem;
+  padding: 3rem;
   display: flex;
   justify-content: center;
   align-items: center;
-  .title-icon {
-    margin: 0rem 2rem;
-    font-size: 1.75rem;
+  h1 {
+    text-shadow: #ffd700 2px 2px;
+    color: #9932cc;
+    padding: 0rem 2rem;
+    font-size: 3rem;
+    font-family: "Snowburst One", cursive;
+  }
+  .hatA {
+    transform: rotate(180deg);
+    transition: 1s ease transform;
+    &:hover {
+      transform: rotate(540deg);
+    }
+  }
+`;
+
+const StyledImg = styled.img`
+  transition: 1s ease transform;
+  &:hover {
+    transform: rotate(360deg);
   }
 `;
 
--- src/components/Wizard.js
+++ src/components/Wizard.js
@@ -1,11 +1,181 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import { FaSearch, FaBackspace } from "react-icons/fa";
+import axios from "axios";
 const Wizard = () => {
+  const [name, setName] = useState("");
+  const [data, setData] = useState([
+    {
+      celcius: 10 + "c",
+      name: "London",
+      humidity: 10,
+      wind: 2,
+      coLon: 50.5,
+      coLat: 50.5,
+    },
+  ]);
+  useEffect(() => {}, []);
+  const fetchHandler = () => {
+    if (name !== "") {
+      const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${name}&appid=f52100a1e77852d08e5b63260b0747d3&units=metric`;
+      axios
+        .get(apiURL)
+        .then((res) => {
+          console.log(res.data);
+          setData({
+            ...data,
+            celcius: res.data.main.temp,
+            name: res.data.name,
+            humidity: res.data.main.humidity,
+            wind: res.data.wind.speed,
+            coLon: res.data.coord.lon,
+            coLat: res.data.coord.lat,
+          });
+        })
+        .catch((err) => console.log(err));
+    }
+  };
+  const clearHandler = () => {
+    let clearInput = document.getElementById("input-box");
+    clearInput.value = "";
+  };
   return (
-    <div className="Title">
-      <h1> Wizard</h1>
-    </div>
+    <StyledBody>
+      <StyledUpperBody>
+        <StyledInput
+          onChange={(e) => setName(e.target.value)}
+          type="text"
+          id="input-box"
+          placeholder="Enter your city here:"
+        ></StyledInput>
+        <InputButtons onClick={fetchHandler}>
+          <FaSearch
+            size={30}
+            style={{
+              backgroundColor: "transparent",
+            }}
+          />
+        </InputButtons>
+        <InputButtons>
+          <FaBackspace
+            onClick={clearHandler}
+            size={30}
+            style={{
+              backgroundColor: "transparent",
+            }}
+          />
+        </InputButtons>
+      </StyledUpperBody>
+      <StyledLowerBody>
+        <Cards>
+          <Card className="city-name">
+            <h3>City Name: </h3>
+            <p>{data.name}</p>
+          </Card>
+          <Card className="city-temp">
+            <h3>Temperature: </h3>
+            <p>{data.celcius}</p>
+          </Card>
+          <Card className="city-lon">
+            <h3>Longitude: </h3>
+            <p>{data.coLon}</p>
+          </Card>
+          <Card className="city-lat">
+            <h3>Latitude: </h3>
+            <p>{data.coLat}</p>
+          </Card>
+          <Card className="city-humidity">
+            <h3>Humidity: </h3>
+            <p>{data.humidity}</p>
+          </Card>
+          <Card className="city-wind">
+            <h3>Wind Speed: </h3>
+            <p>{data.wind}</p>
+          </Card>
+        </Cards>
+      </StyledLowerBody>
+    </StyledBody>
   );
 };
+
+const StyledBody = styled.div`
+  //display: flex;
+  //align-items: center;
+  //justify-content: center;
+`;
+
+const StyledUpperBody = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const StyledLowerBody = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const Cards = styled.div`
+  margin-top: 3rem;
+  width: 70vw;
+  //display: flex;
+  //display: grid;
+  //grid-template-columns: auto auto auto;
+`;
+
+const Card = styled.div`
+  display: flex;
+  color: ghostwhite;
+  align-items: center;
+  height: 3rem;
+  width: 20rem;
+  border: 2px solid ghostwhite;
+  border-radius: 10px;
+  padding: 0.5rem;
+  margin-bottom: 1rem;
+  h3 {
+    width: 9rem;
+  }
+  p {
+    padding-top: 0.2rem;
+    padding-left: 1rem;
+  }
+`;
+
+const StyledInput = styled.input`
+  font-family: "Manrope", sans-serif;
+  font-weight: bold;
+  background-color: ghostwhite;
+  color: darkgray;
+  width: 30em;
+  height: 2.5rem;
+  padding: 0.5rem;
+  border-radius: 10px;
+  border: 2px solid #32174d;
+  font-size: 1.75rem;
+  color: #32174d;
+  margin: 0.5rem;
+`;
+
+const InputButtons = styled.div`
+  background-color: ghostwhite;
+  color: #32174d;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 2.5rem;
+  height: 2.5rem;
+  padding: 0.5rem;
+  border: 2px solid #32174d;
+  border-radius: 10px;
+  margin: 0.5rem;
+  transition: 0.5s all ease;
+  &:hover {
+    cursor: pointer;
+    color: #ffd700;
+    background-color: #32174d;
+    border: 2px solid #ffd700;
+  }
+`;
 
 export default Wizard;
